39dfced9cc325c3e9c635ecb7583de8d95e93b55
Claudiu <claudiu@claudiupersoiu.ro>
2014-12-10 22:26:12 +0200

    led tests

---
+++ led_dim_timed.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+led_pin = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led_pin, GPIO.OUT)
+pwm_led = GPIO.PWM(led_pin, 500)
+pwm_led.start(100)
+
+try:
+	while True:
+		for i in range(100):
+			time.sleep(0.01)
+			pwm_led.ChangeDutyCycle(i)
+
+		for i in range(100):
+			time.sleep(0.01)
+			pwm_led.ChangeDutyCycle(100 - i)
+finally:
+	GPIO.cleanup()
