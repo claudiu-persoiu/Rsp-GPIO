ff2baf2ce3c14d6e7c2a168dc0efbf2c22a26b42
Claudiu <claudiu@claudiupersoiu.ro>
2014-12-20 00:49:55 +0200

    update 8 segment

--- 8fragment_74HC595N.py
+++ 8fragment_74HC595N.py
@@ -1,62 +1,37 @@
-import RPi.GPIO as GPIO
 import time
-
+from Shifter import Shifter
 
 class Display8egment:
     
     options = {
-        0 : '01111110',
-        1 : '01001000',
-        2 : '00111101',
-        3 : '01101101',
-        4 : '01001011',
-        5 : '01100111',
-        6 : '01110111',
-        7 : '01001100',
-        8 : '01111111',
-        9 : '01001111',
+        '0' : '01111110',
+        '1' : '01001000',
+        '2' : '00111101',
+        '3' : '01101101',
+        '4' : '01001011',
+        '5' : '01100111',
+        '6' : '01110111',
+        '7' : '01001100',
+        '8' : '01111111',
+        '9' : '01001111',
         'r': '00000000'
     }
     
-    def __init__(self, data_in = 11, latch = 12, clock = 13):
-        self.data_in = data_in
-        self.latch = latch
-        self.clock = clock
-        
-        GPIO.setmode(GPIO.BOARD)
-        
-        GPIO.setup(self.data_in, GPIO.OUT)
-        GPIO.setup(self.latch, GPIO.OUT)
-        GPIO.setup(self.clock, GPIO.OUT)
+    def __init__(self):
+        self.shifter = Shifter()
     
-    def display(self, char):
-        for c in self.__class__.options[char]:
-            if c is '1':
-                GPIO.output(self.data_in,GPIO.HIGH)
-            else:
-                GPIO.output(self.data_in,GPIO.LOW)
-                
-            self._tick()    
+    def display(self, string):
+        out = ''
+        for c in str(string)[::-1]:
+            out += self.__class__.options[c]
             
-        GPIO.output(self.data_in, GPIO.LOW)
-        self._display()
-    
-    def _tick(self):
-        GPIO.output(self.clock, GPIO.HIGH)
-        GPIO.output(self.clock, GPIO.LOW)
-    
-    def _display(self):
-        GPIO.output(self.latch,GPIO.HIGH)
-        GPIO.output(self.latch,GPIO.LOW)
-    
-    def clean(self):
-        GPIO.cleanup()
+        self.shifter.display(out)
         
 
 display = Display8egment()
-for i in range(0, 10):
+for i in range(10, 30)[::-1]:
     display.display(i)
     time.sleep(1)
 
-display.display('r')
-display.clean()
+display.display('rr')
+# display.clean()
---
+++ Shifter.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+
+class Shifter:
+    
+    def __init__(self, data_in = 11, latch = 12, clock = 13):
+        self.data_in = data_in
+        self.latch = latch
+        self.clock = clock
+        
+        GPIO.setmode(GPIO.BOARD)
+        
+        GPIO.setup(self.data_in, GPIO.OUT)
+        GPIO.setup(self.latch, GPIO.OUT)
+        GPIO.setup(self.clock, GPIO.OUT)
+    
+    def display(self, str):
+        for c in str:
+            if c is '1':
+                GPIO.output(self.data_in,GPIO.HIGH)
+            else:
+                GPIO.output(self.data_in,GPIO.LOW)
+            self._tick()    
+            
+        GPIO.output(self.data_in, GPIO.LOW)
+        self._display()
+    
+    def _tick(self):
+        GPIO.output(self.clock, GPIO.HIGH)
+        GPIO.output(self.clock, GPIO.LOW)
+    
+    def _display(self):
+        GPIO.output(self.latch,GPIO.HIGH)
+        GPIO.output(self.latch,GPIO.LOW)
+    
+    def __del__(self):
+        GPIO.cleanup()
