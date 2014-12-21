65816a93d5a96fb53eb22e81de7d68c35dc14b58
Claudiu <claudiu@claudiupersoiu.ro>
2014-12-21 13:53:13 +0200

    added dot matrix tree example

---
+++ dot_matrix_tree.py
@@ -0,0 +1,15 @@
+from DisplayMatrix import DisplayMatrix
+
+display = DisplayMatrix()
+
+while True:
+    display.display([
+        '11101111',
+        '11000111',
+        '10000011',
+        '11000111',
+        '10000011',
+        '00000001',
+        '11101111',
+        '11111111'
+    ])
