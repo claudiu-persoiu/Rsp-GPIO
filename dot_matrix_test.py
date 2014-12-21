2e9fab5c8111af30513640374fd6b98d9a428ce4
Claudiu <claudiu@claudiupersoiu.ro>
2014-12-21 22:39:30 +0200

    added a more interactive text for dot matrix

--- dot_matrix_test.py
+++ dot_matrix_test.py
@@ -1,4 +1,15 @@
 from DisplayMatrixText import DisplayMatrixText
 
 display_text = DisplayMatrixText()
-display_text.display('the quick brown fox jumps over the lazy dog')
+
+try:
+    while True:
+        text = raw_input("Text to be displayed: ")
+        print "Displaying..."
+        display_text.display(text)
+except Exception:
+    pass
+except KeyboardInterrupt:
+    pass
+finally:
+    print "Bye bye!"
